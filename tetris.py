603b33d236947af07521da6890248dec8e5fc290
monicagangwar <monicagangwar@hotmail.com>
2016-07-06 15:01:47 +0530

    basic display code

--- tetris.py
+++ tetris.py
@@ -3,6 +3,7 @@
 
 import sys
 import pygame
+from tetris import const,state
 
 from pygame.locals import *
 
@@ -10,10 +11,40 @@ def terminate(error=0):
 	pygame.quit()
 	sys.exit(error)
 
+def draw_screen(surface,game_state):
+
+	#Draw well with border
+	pygame.draw.rect(surface,const.C_LGRAY,(const.MARGIN_LEFT - 5,const.MARGIN_TOP - 5,
+																					const.WELL_PX_W + 10,const.WELL_PX_H + 10))
+
+	pygame.draw.rect(surface,const.C_DGRAY,(const.MARGIN_LEFT,const.MARGIN_TOP,
+																					const.WELL_PX_W,const.WELL_PX_H))
+
+	#Draw blocks
+	y = 0
+	while y < const.WELL_H:
+		x = 0
+		while x < const.WELL_W:
+			curr = game_state.well[y][x]
+			if curr != 0:
+				current_color = const.C_LIST[curr]
+				pygame.draw.rect(surface,current_color,(const.MARGIN_LEFT + x * const.BLOCK_SIZE,
+																								const.MARIN_TOP + y * const.BLOCK_SIZE,
+																								const.BLOCK_SIZE,const.BLOCK_SIZE))
+			x += 1
+		y += 1
+	
+	pygame.display.flip()
+
 def main():
 	pygame.init()
-	pygame.display.set_mode((400,500))
+	displaysurf = pygame.display.set_mode((const.SCR_W,const.SCR_H))
+	game_state = state.GameState()
 	while True:
+		#display
+
+		draw_screen(displaysurf,game_state)
+
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				terminate()
---
+++ tetris/state.py
@@ -0,0 +1,33 @@
+from tetris import const
+import random
+
+class GameState(object):
+	def __init__(self):
+		super(GameState,self).__init__()
+		self.init_well()
+		self.level = 1
+		self.score = 0
+		self.cur_piece = None
+		self.next_piece = None
+	
+	def init_well(self):
+		self.well = []
+		for y in range(0,const.WELL_H):
+			row = []
+			for x in range(0,const.WELL_W):
+				row.append(0)
+			self.well.append(row)
+
+	def line_cleared(self):
+		self.score += 1
+		if self.score % 10 == 0 and self.score > 0:
+			self.level += 1
+	
+	def get_new_piece(self):
+		return [[random.choice(range(1,8))]]
+
+	def spawn_piece(self):
+		if self.next_piece == None:
+			self.next_piece = self.get_new_piece()
+		self.cur_piece = self.next_piece
+		self.next_piece = self.get_new_piece()
